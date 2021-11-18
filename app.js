949ed6220c02d1ece575a5c1e82848a64b94807b
abrahamls <abraham.siahaan1997@gmail.com>
2021-11-18 14:31:10 +0700

    deploy

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 if (process.env.NODE_ENV !== "production") {
-  require('dotenv').config()
+  require("dotenv").config();
 }
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT ||  3000
-const cors = require('cors')
-const router = require('./routes/index')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+const router = require("./routes/index");
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+const corsOptions = {
+  origin: "https://p2-repeat-iproject.web.app",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
-app.use(router)
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(router);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
